3c258aec45758f9bd339a704b92b5cdd8f060b89
ddukss <pignib@udesa.edu.ar>
2025-09-16 11:24:20 -0300

    Sistema de favoritos, quitar favoritos y agregarlos

--- src/components/CardPelicula/CardPelicula.js
+++ src/components/CardPelicula/CardPelicula.js
@@ -8,7 +8,23 @@ class CardPelicula extends Component {
     this.state = {
       verDescripcion: false,
       textoBoton: 'Ver descripción',
+      esFavorito: false
     };
+  }
+
+  componentDidMount() {
+    const id = this.props.id;
+
+    let favoritos = [];
+    let datosEnLocalStorage = localStorage.getItem('LSfavoritos');
+    if (datosEnLocalStorage != null) {
+      favoritos = JSON.parse(datosEnLocalStorage);
+    }
+
+    const yaEsta = favoritos.filter(function (unID) { return unID === id; });
+    if (yaEsta.length > 0) {
+      this.setState({ esFavorito: true });
+    }
   }
 
   manejarDescripcion = () => {
@@ -18,32 +34,79 @@ class CardPelicula extends Component {
     }));
   };
 
+  agregarAFavoritos = () => {
+    const id = this.props.id;
+
+    let favoritos = [];
+    let datosEnLocalStorage = localStorage.getItem('LSfavoritos');
+    if (datosEnLocalStorage != null) {
+      favoritos = JSON.parse(datosEnLocalStorage);
+    }
+
+    const yaEsta = favoritos.filter(function (unID) { return unID === id; });
+    if (yaEsta.length === 0) {
+      favoritos.push(id); 
+      localStorage.setItem('LSfavoritos', JSON.stringify(favoritos));
+      this.setState({ esFavorito: true }); 
+    }
+  };
+
+  quitarDeFavoritos = () => {
+    const id = this.props.id;
+
+    let favoritos = [];
+    let datosEnLocalStorage = localStorage.getItem('LSfavoritos');
+    if (datosEnLocalStorage != null) {
+      favoritos = JSON.parse(datosEnLocalStorage);
+    }
+
+    const favoritosActualizados = favoritos.filter(function (unID) { return unID !== id; });
+    localStorage.setItem('LSfavoritos', JSON.stringify(favoritosActualizados));
+    this.setState({ esFavorito: false });
+  };
+
   render() {
     const { id, titulo, imagen, descripcion, tipo } = this.props;
-    
-    const urlImagen = `https://image.tmdb.org/t/p/w500/${imagen}`;
+
+    // URL de imagen (sin usar ||)
+    let urlImagen = '';
+    if (imagen) {
+      urlImagen = `https://image.tmdb.org/t/p/w500/${imagen}`;
+    }
 
     return (
       <article className="single-card-movie">
-        <img src={urlImagen} className="card-img-top" alt={titulo} />
+        {urlImagen ? (
+          <img src={urlImagen} className="card-img-top" alt={titulo} />
+        ) : (
+          <div className="no-image">Sin imagen</div>
+        )}
+
         <div className="cardBody">
           <h5 className="card-title">{titulo}</h5>
-          <section className= "section-desc">
-          {this.state.verDescripcion && (
-            <p className="card-text">{descripcion}</p>
-          )}
 
-          <button className="btn btn-primary" onClick={this.manejarDescripcion}>
-            {this.state.textoBoton}
-          </button>
-          
-          <Link to={`/${tipo}/${id}`} className="btn btn-primary">
-            Ir a detalle
-          </Link>
-          
-          <button className="btn alert-primary">
-            ♥️
-          </button>
+          <section className="section-desc">
+            {this.state.verDescripcion && (
+              <p className="card-text">{descripcion}</p>
+            )}
+
+            <button className="btn btn-primary" onClick={this.manejarDescripcion}>
+              {this.state.textoBoton}
+            </button>
+
+            <Link to={`/${tipo}/${id}`} className="btn btn-primary">
+              Ir a detalle
+            </Link>
+
+            {this.state.esFavorito ? (
+              <button className="btn alert-primary" onClick={this.quitarDeFavoritos} title="Quitar de favoritos">
+                ♥️
+              </button>
+            ) : (
+              <button className="btn alert-primary" onClick={this.agregarAFavoritos} title="Agregar a favoritos">
+                ♥️
+              </button>
+            )}
           </section>
         </div>
       </article>
